23a856fc16452ed0c4c773faebeea544bd0144a3
Joe <joebbaney@gmail.com>
2025-01-10 14:34:53 -0600

    finished Data Types chapter of Intro to Python Course

---
+++ data_types.py
@@ -0,0 +1,24 @@
+# Exercises
+#1. Identify the data type or class for each of the following values:
+    # 'True' -- String
+    # False -- Bool
+    # (1, 2, 3) -- Tuple
+    # 1.5 -- Float
+    # [1, 2, 3] -- List
+    # 2 -- Int
+    # range(5) -- Range
+    # {1, 2, 3} -- Set
+    # None -- NoneType
+    # {'foo': 'bar'} -- dict
+
+#2. Create a tuple called names that contains several pet names
+names = ("Asta", "Butterscotch", "Pudding", "Neptune", "Darwin")
+
+#3. Create a dictionary named pets that contains a list of pet names and the type of animal
+pets = {
+    "Asta": "dog",
+    "Butterscotch": "cat",
+    "Pudding": "cat",
+    "Neptune": "fish",
+    "Darwin": "lizard"
+}
